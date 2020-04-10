b7648942374bcd3d0d9d8b8eadb1e80da1e76074
sol <mssarratea@gmail.com>
2020-04-10 14:01:32 -0400

    Fixup: relative paths
    temporal solution

--- src/server.js
+++ src/server.js
@@ -4,27 +4,24 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/js'));
+app.use(express.static(__dirname + '/node_modules/three'));
+app.use(express.static(__dirname + '/node_modules/three/build'));
+app.use(express.static(__dirname + '/node_modules/dat.gui/build'));
+app.use(express.static(__dirname + '/node_modules/three/examples/js/controls'));
+
+app.use('/', router);
+
 const router = express.Router();
-router.get('/', (req, res) => {
-    console.log( __dirname )
-    res.sendFile("public/index.html");
-});
+
 router.get("/mandelbulb", (request, response) => {
-    response.sendFile("public/mandelbulb.html");
+    response.sendFile('../public/mandelbulb.html');
   });
   
-  router.get("/mandelbrot", (request, response) => {
-    response.sendFile("public/mandelbrot.html");
-  });
-
-app.use(express.static('public'),router);
-app.use(express.static('public/js')),router;
-
-app.use(express.static('node_modules/three'),router);
-app.use(express.static('node_modules/three/build'),router);
-app.use(express.static('node_modules/dat.gui/build'),router);
-app.use(express.static('node_modules/three/examples/js/controls'),router);
-
+router.get("/mandelbrot", (request, response) => {
+  response.sendFile('../public/mandelbrot.html');
+});
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);
