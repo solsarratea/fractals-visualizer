6626b364068faf73da596f48507bcace3bcff49a
sol <mssarratea@gmail.com>
2020-04-10 13:42:40 -0400

    Fix paths

--- src/server.js
+++ src/server.js
@@ -6,27 +6,28 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+    console.log( __dirname )
+    res.sendFile("public/index.html");
 });
 router.get("/mandelbulb", (request, response) => {
-    response.sendFile(__dirname + "/public/mandelbulb.html");
+    response.sendFile("public/mandelbulb.html");
   });
   
   router.get("/mandelbrot", (request, response) => {
-    response.sendFile(__dirname + "/public/mandelbrot.html");
+    response.sendFile("public/mandelbrot.html");
   });
 
-app.use(express.static('public'));
-app.use(express.static('public/js'));
+app.use(express.static('public'),router);
+app.use(express.static('public/js')),router;
 
-app.use(express.static(__dirname +'node_modules/three'));
-app.use(express.static(__dirname +'node_modules/three/build'));
-app.use(express.static(__dirname +'/node_modules/dat.gui/build'));
-app.use(express.static(__dirname +'/node_modules/three/examples/js/controls'));
+app.use(express.static('node_modules/three'),router);
+app.use(express.static('node_modules/three/build'),router);
+app.use(express.static('node_modules/dat.gui/build'),router);
+app.use(express.static('node_modules/three/examples/js/controls'),router);
 
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/.netlify/functions/server', router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
