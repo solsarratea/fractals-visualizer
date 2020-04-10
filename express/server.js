574a3351aa3088b5d14d729d1e61e45fe157b4af
sol <mssarratea@gmail.com>
2020-04-10 12:55:15 -0400

    Rename server folder; fixup

--- express/server.js
+++ express/server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-    response.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/public/index.html");
 });
 router.get("/mandelbulb", (request, response) => {
     response.sendFile(__dirname + "/public/mandelbulb.html");
